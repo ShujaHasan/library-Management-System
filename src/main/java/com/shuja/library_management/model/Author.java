570ffbbc0ba61de6401dc95fb0217695eca740b5
Shuja Hasan <shujahassan655@gmail.com>
2026-07-25 20:51:47 +0500

    feat: add Author entity for library management

---
+++ src/main/java/com/shuja/library_management/model/Author.java
@@ -0,0 +1,75 @@
+package com.shuja.library_management.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email format")
+    private String email;
+
+    @NotBlank
+    private String country;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+
+    public Author(){}
+
+    public Author(String name, String email, String country){
+        this.name = name;
+        this.email = email;
+        this.country = country;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
