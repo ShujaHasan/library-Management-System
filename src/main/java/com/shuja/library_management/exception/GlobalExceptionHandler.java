69598db2e42ed04c69278b91e710288fbff782b8
Shuja Hasan <shujahassan655@gmail.com>
2026-07-28 20:16:56 +0500

    feat: handle bean validation errors globally with custom response

--- src/main/java/com/shuja/library_management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shuja/library_management/exception/GlobalExceptionHandler.java
@@ -1,22 +1,45 @@
 package com.shuja.library_management.exception;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    @RestControllerAdvice
-    public class AuthorNotFoundException {
-        @ExceptionHandler(BookNotFoundException.class)
+    @ExceptionHandler(BookNotFoundException.class)
         public ResponseEntity<String> handleBookNotFound(BookNotFoundException ex){
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(ex.getMessage());
         }
+
+        @ExceptionHandler(AuthorNotFoundException.class)
+        public ResponseEntity<String> handleAuthorNotFound(AuthorNotFoundException exception){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(exception.getMessage());
+        }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errors);
     }
+
+
+
+
+
 
 }
