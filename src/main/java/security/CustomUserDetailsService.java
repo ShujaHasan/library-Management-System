b6b529f8afaa8d18d0856d4b0e02c7185afe111f
Shuja Hasan <shujahassan655@gmail.com>
2026-07-30 22:30:21 +0500

    feat: implement CustomUserDetailsService for database authentication

---
+++ src/main/java/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package security;
+
+import com.shuja.library_management.model.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import com.shuja.library_management.exception.UsernameNotFoundException;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "Username " + username + " not found"
+                        ));
+
+        return new CustomUserDetails(user);
+    }
+
+}
