f379bb9de038c56f88ba8e39bc09ade91a81e5ae
Shuja Hasan <shujahassan655@gmail.com>
2026-08-04 11:38:44 +0500

    feat: implement AuthService with register and login functionality

--- src/main/java/com/shuja/library_management/service/AuthService.java
+++ src/main/java/com/shuja/library_management/service/AuthService.java
@@ -1,23 +1,34 @@
 package com.shuja.library_management.service;
 
+import com.shuja.library_management.dto.LoginRequestDTO;
+import com.shuja.library_management.dto.LoginResponseDTO;
 import com.shuja.library_management.dto.RegisterRequestDTO;
 import com.shuja.library_management.dto.UserResponseDTO;
 import com.shuja.library_management.exception.UsernameAlreadyExistsException;
 import com.shuja.library_management.model.User;
 import com.shuja.library_management.model.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import security.JwtService;
 
 @Service
-public class RegistrationService {
+public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
 
-    public RegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
     public UserResponseDTO convertTODTO(User user){
@@ -44,6 +55,23 @@ public class RegistrationService {
         User savedUser = userRepository.save(user);
 
         return convertTODTO(savedUser);
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO dto){
+        Authentication authentication =
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        dto.getUsername(),
+                        dto.getPassword()
+                )
+        );
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        String token = jwtService.generateToken(userDetails);
+
+        return new LoginResponseDTO(token);
 
     }
+
 }
