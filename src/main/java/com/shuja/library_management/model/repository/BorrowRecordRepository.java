b493f81cf2434522df743f23aebd6f3123782dc2
Shuja Hasan <shujahassan655@gmail.com>
2026-08-07 20:59:33 +0500

    feat(repository): add findByMemberIdAndBookIdAndStatus to BorrowRecordRepository

---
+++ src/main/java/com/shuja/library_management/model/repository/BorrowRecordRepository.java
@@ -0,0 +1,24 @@
+package com.shuja.library_management.model.repository;
+
+import com.shuja.library_management.model.Book;
+import com.shuja.library_management.model.BorrowRecord;
+import com.shuja.library_management.model.BorrowStatus;
+import com.shuja.library_management.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Integer> {
+    boolean existsByMemberAndBookAndStatus(
+            Member member,
+            Book book,
+            BorrowStatus status
+    );
+
+    Optional<BorrowRecord> findByMemberIdAndBookIdAndStatus(
+            Integer memberId,
+            Integer bookId,
+            BorrowStatus status
+    );
+
+
+}
