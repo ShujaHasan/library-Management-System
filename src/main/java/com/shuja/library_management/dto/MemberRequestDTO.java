3b17fefe6d46528451f6c9ecce9174b5875d30a2
Shuja Hasan <shujahassan655@gmail.com>
2026-08-12 21:19:44 +0500

    feat(dto): add MemberRequestDTO

--- src/main/java/com/shuja/library_management/dto/MemberRequestDTO.java
+++ src/main/java/com/shuja/library_management/dto/MemberRequestDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class MemberRequestDTO {
+
     @NotBlank(message = "Name cannot be empty")
     private String fullName;
 
@@ -11,9 +12,56 @@ public class MemberRequestDTO {
     @NotBlank(message = "Email cannot be empty")
     private String email;
 
-    @NotBlank(message = "Phone Number cannot be empty!")
+    @NotBlank(message = "Phone number cannot be empty")
     private String phone;
 
     private String address;
 
+    public MemberRequestDTO() {
+    }
+
+    public MemberRequestDTO(String fullName,
+                            String email,
+                            String phone,
+                            String address) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    // getters and setters
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
