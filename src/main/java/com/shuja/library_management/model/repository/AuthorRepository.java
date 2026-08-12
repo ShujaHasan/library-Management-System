1f9dba1ecee1bc86035ddee02b7df6e4457f3fc9
Shuja Hasan <shujahassan655@gmail.com>
2026-08-12 21:12:07 +0500

    feat: create Author Repository

--- src/main/java/com/shuja/library_management/model/repository/AuthorRepository.java
+++ src/main/java/com/shuja/library_management/model/repository/AuthorRepository.java
@@ -4,4 +4,6 @@ import com.shuja.library_management.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    boolean existsByEmail(String email);
+
 }
