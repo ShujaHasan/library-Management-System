881869c5760bc26b4bb1814545fb69ced7b84494
Shuja Hasan <shujahassan655@gmail.com>
2026-08-12 21:19:29 +0500

    feat(repository): add MemberRepository

--- src/main/java/com/shuja/library_management/model/repository/MemberRepository.java
+++ src/main/java/com/shuja/library_management/model/repository/MemberRepository.java
@@ -4,4 +4,8 @@ import com.shuja.library_management.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
 }
