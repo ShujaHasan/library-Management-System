e88a53e20d89e9ec45354b4b0c50ecceb758c595
Shuja Hasan <shujahassan655@gmail.com>
2026-08-18 23:33:18 +0500

    feat(dashboard): add query for total available copies in BookRepository

--- src/main/java/com/shuja/library_management/model/repository/BookRepository.java
+++ src/main/java/com/shuja/library_management/model/repository/BookRepository.java
@@ -2,6 +2,12 @@ package com.shuja.library_management.model.repository;
 
 import com.shuja.library_management.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    @Query("""
+    SELECT COALESCE(SUM(b.availableCopies), 0)
+    FROM Book b
+""")
+    long getTotalAvailableCopies();
 }
